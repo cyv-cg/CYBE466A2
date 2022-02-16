4e43eaa27f3a037014ae4ea470bdd4df15d84e87
trk0012 <72991381+trk0012@users.noreply.github.com>
2022-02-16 09:49:13 -0500

    Started change

--- rule.py
+++ rule.py
@@ -1,3 +1,11 @@
+import resource
+
+
 class rule():
-	def __init__():
-		print("rule __init__")
+	def __init__(self, userAttribute, resourceAttribute, actions, constraints):
+		self.usAttrib = userAttribute
+		self.reAttrib = resourceAttribute
+		self.actions = actions
+		self.constraint = constraints
+		
+	
