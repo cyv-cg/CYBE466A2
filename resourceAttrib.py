686587194155e6d0295b79c84a0df13fe0c9071b
cyvet <cyvet.cs@gmail.com>
2022-02-25 09:36:59 -0500

    id's are now part of attributes dictionary

--- resourceAttrib.py
+++ resourceAttrib.py
@@ -1,11 +1,10 @@
 import abac_parser as p
 
 class resourceAttrib():
-	rid = ""
 
 	attributes = { }
 
 	def __init__(self, rid, attributes):
-		self.rid = rid
 		self.attributes = p.parse_attrib(attributes)
-		print(f"resourceAttrib __init__({self.rid}, {self.attributes})")
+		self.attributes['rid'] = rid
+		print(f"resourceAttrib __init__({self.attributes['rid']}, {self.attributes})")
--- userAttrib.py
+++ userAttrib.py
@@ -1,11 +1,9 @@
 import abac_parser as p
 
 class userAttrib():
-	uid = ""
-
 	attributes = { }
 
 	def __init__(self, uid, attributes):
-		self.uid = uid
 		self.attributes = p.parse_attrib(attributes)
-		print(f"userAttrib __init__({self.uid}, {self.attributes})")
+		self.attributes['uid'] = uid
+		print(f"userAttrib __init__({self.attributes['uid']}, {self.attributes})")
